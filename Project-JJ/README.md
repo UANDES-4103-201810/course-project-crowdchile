e7e4151247b03bb5b205f063cca4c5d18c925c5c
Juan Valdivieso <Javaldivieso>
2018-04-13 22:38:04 -0300

    Staticpage

--- Project-JJ/README.md
+++ Project-JJ/README.md
@@ -1,5 +1,5 @@
 # README
-
+Juan Valdivieso and Juan Manuel Campos
 This README would normally document whatever steps are necessary to get the
 application up and running.
 
--- Project-JJ/config/routes.rb
+++ Project-JJ/config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   resources :promises
   resources :admins
   resources :user_profiles
+  root 'application#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
