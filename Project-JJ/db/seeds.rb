3f31773510697760ae1ebf5cae76d21e9cd66a10
Juanma <jcampos@miuandes.cl>
2018-05-05 18:38:20 -0300

    Seeds

--- Project-JJ/db/seeds.rb
+++ Project-JJ/db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category.create(name: "Music")
+Category.create(name: "Animals")
+user = User.new
+user.email= "javaldivieso1@miuandes.cl"
+user.password = "lala123"
+user.save
+Project.create(title: "BitClinic", description: "Easy clinic transactions", donationamount: 1, category: "Medical", user_id: 1)
+Project.create(title: "Save the Meerkats", description: "Save the meerkats by helping our cause", donationamount: 1, category: "Animals", user_id: 1)
+
