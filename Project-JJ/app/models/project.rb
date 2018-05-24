ba24a3ed597f30da375b26a84d0185b1244c398d
JuanmaCampos <jcampos@miuandes.cl>
2018-05-24 17:56:27 -0400

    Image upload, css profile

--- Project-JJ/app/models/project.rb
+++ Project-JJ/app/models/project.rb
@@ -6,6 +6,8 @@ class Project < ApplicationRecord
 	validates :title, presence: true
 	validates :category_id, presence: true
 	validates :description, presence: true
+	has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", large: "600x600>"}
+	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 	
 
 	def self.search(search)
