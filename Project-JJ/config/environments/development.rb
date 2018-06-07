e1ae9b1e49a3259810feb2cf09953dd1f29e86d1
Juan Valdivieso <Javaldivieso>
2018-06-07 19:38:48 -0400

    Mailer

--- Project-JJ/config/environments/development.rb
+++ Project-JJ/config/environments/development.rb
@@ -32,9 +32,19 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => "587",
+    :domain => "example.com",
+    :user_name => "",
+    :password => "",
+    :authentication => 'plain',
+    :enable_starttls_auto => true
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -51,7 +61,8 @@ Rails.application.configure do
   config.assets.debug = true
 
   # Suppress logger output for asset requests.
-  config.assets.quiet = true
+  
+   
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
---
+++ Project-JJ/config/initializers/smtp_settings.rb
@@ -0,0 +1,9 @@
+ActionMailer::Base.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => "587",
+    :domain => "example.com",
+    :user_name => "javaldivieso1@miuandes.cl",
+    :password => "oleole120671",
+    :authentication => 'plain',
+    :enable_starttls_auto => true
+}
