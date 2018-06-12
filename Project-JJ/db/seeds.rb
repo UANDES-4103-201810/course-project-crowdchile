96b261d1b73cda0f7cb3c7075683debb764f3a04
Juan Valdivieso <Javaldivieso>
2018-06-12 19:29:19 -0400

    index

--- Project-JJ/db/seeds.rb
+++ Project-JJ/db/seeds.rb
@@ -20,13 +20,47 @@ Category.create(name: "Applications")
 
 #Admin seeds
 user = User.new
-user.email = 'admin@example1.com'
+user.email = 'admin@example221.com'
 user.password = 'admin123'
 user.password_confirmation = 'admin123'
 user.admin = true
 user.save!
 
+imagendata = File.new('/home/juan/Desktop/course-project-crowdchile/Project-JJ/db/Cesme-700x400.jpg')
+project = Project.new
+project.title = "EasyVacation" 
+project.description = 'An easy and cheap way to go to the places you dream of. Hop on!'
+project.user_id = 1
+project.image = imagendata
+project.mark = true
+project.category_id = 1
+project.donationamount = 100
+project.deliverydate = Time.now
+project.save!
 
+imagendata = File.new('/home/juan/Desktop/course-project-crowdchile/Project-JJ/db/meerkat-1309608-700x400.jpg')
+project = Project.new
+project.title = "Save the meerkats" 
+project.description = 'Meerkats are endangered species from Africa. Help us with our project and help them with their lifestyle.'
+project.user_id = 1
+project.image = imagendata
+project.mark = true
+project.category_id = 1
+project.donationamount = 100
+project.deliverydate = Time.now
+project.save!
+
+imagendata = File.new('/home/juan/Desktop/course-project-crowdchile/Project-JJ/db/plo.jpg')
+project = Project.new
+project.title = "MobileMe" 
+project.description = 'Everyone in the world deserves to be connected. With MobileMe, it is easier to access all kinds of technology all around the world.'
+project.user_id = 1
+project.image = imagendata
+project.mark = true
+project.category_id = 1
+project.donationamount = 100
+project.deliverydate = Time.now
+project.save!
 
 
 
